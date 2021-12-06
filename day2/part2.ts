2468571dbc7659f6e28712d881cde6f61b8f440c
Liudas Kazalupskis <lkazalupskis@kayak.com>
2021-12-06 06:19:48 +0200

    AOC - day 2 part 2 - done

--- day2/part2.ts
+++ day2/part2.ts
@@ -6,16 +6,18 @@ const UP_COMMAND = "up";
 
 let x = 0;
 let y = 0;
+let aim = 0;
 
 controlsData.forEach((order => {
     if (order.cmd === FORWARD_COMMAND) {
         x += order.value;
+        y += order.value * aim;
     }
     if (order.cmd === DOWN_COMMAND) {
-        y += order.value;
+        aim += order.value;
     }
     if (order.cmd === UP_COMMAND) {
-        y -= order.value;
+        aim -= order.value;
     }
 }))
 
